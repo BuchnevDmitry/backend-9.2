cf17e0e9358231e0760c3cd9ec4a9761844939f6
BuchnevDmitry <buchnev03.03@mail.ru>
2024-04-21 21:53:05 +0300

    feature(RT-46): first implementation CategoryController

--- rentool/tool/src/main/java/com/edu/tool/model/Category.java
+++ rentool/tool/src/main/java/com/edu/tool/model/Category.java
@@ -1,6 +1,8 @@
 package com.edu.tool.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
@@ -12,6 +14,7 @@ import lombok.Setter;
 @Setter
 public class Category {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
