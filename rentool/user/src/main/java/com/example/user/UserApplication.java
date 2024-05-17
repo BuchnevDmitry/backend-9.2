4b8a6372e254c3eda2a8c40e59c96046f78071ce
BuchnevDmitry <buchnev03.03@mail.ru>
2024-05-17 23:44:25 +0300

    feature(RT-63): add migration to user

--- rentool/user/src/main/java/com/example/user/UserApplication.java
+++ rentool/user/src/main/java/com/example/user/UserApplication.java
@@ -1,9 +1,12 @@
 package com.example.user;
 
+import com.example.user.configuration.KeycloakProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({KeycloakProperties.class})
 public class UserApplication {
 
     public static void main(String[] args) {
