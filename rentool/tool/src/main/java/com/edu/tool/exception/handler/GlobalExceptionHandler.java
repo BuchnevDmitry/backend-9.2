c0045021a4ab12e521c67e62201536aa31e03223
BuchnevDmitry <buchnev03.03@mail.ru>
2024-06-15 18:34:05 +0300

    feature(RT-75): add BadRequestException

--- rentool/tool/src/main/java/com/edu/tool/exception/handler/GlobalExceptionHandler.java
+++ rentool/tool/src/main/java/com/edu/tool/exception/handler/GlobalExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.HandlerMethodValidationException;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @RestControllerAdvice
@@ -17,7 +18,7 @@ public class GlobalExceptionHandler {
             responseCode = "400",
             description = "Некорректные параметры запроса")
     })
-    @ExceptionHandler({HttpMessageNotReadableException.class, MethodArgumentTypeMismatchException.class})
+    @ExceptionHandler({HttpMessageNotReadableException.class, MethodArgumentTypeMismatchException.class, HandlerMethodValidationException.class})
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ApiErrorResponse handleBadRequestException() {
         return new ApiErrorResponse(
