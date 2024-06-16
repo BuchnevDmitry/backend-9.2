5b76b416e474987e90ec27fa7efd55da072f8b8a
BuchnevDmitry <buchnev03.03@mail.ru>
2024-06-17 02:22:02 +0300

    fix(RT-80): fix bug with swagger

--- rentool/tool/src/main/java/com/edu/tool/ToolApplication.java
+++ rentool/tool/src/main/java/com/edu/tool/ToolApplication.java
@@ -1,11 +1,12 @@
 package com.edu.tool;
 
+import com.edu.tool.configuration.ApplicationConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties()
+@EnableConfigurationProperties({ApplicationConfig.class})
 public class ToolApplication {
 
     public static void main(String[] args) {
