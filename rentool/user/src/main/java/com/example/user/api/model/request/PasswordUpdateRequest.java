b13051a0016155d8679f5d45631b2cdd56087516
BuchnevDmitry <buchnev03.03@mail.ru>
2024-06-19 04:38:38 +0300

    feature(RT-85): fix PasswordUpdateRequest

--- rentool/user/src/main/java/com/example/user/api/model/request/PasswordUpdateRequest.java
+++ rentool/user/src/main/java/com/example/user/api/model/request/PasswordUpdateRequest.java
@@ -1,13 +1,9 @@
 package com.example.user.api.model.request;
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
 
 public record PasswordUpdateRequest(
     @NotEmpty
-    @Min(1)
-    @Max(100)
     String password
 ) {
 }
